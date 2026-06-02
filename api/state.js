9ff4a0972c576cbeb1677029a68b777bcf050ea0
facurodriguez06 <facundorodriguezsp@gmail.com>
2026-06-01 22:25:36 -0300

    fix: add CORS headers to backend response to allow Capacitor/cross-origin requests

--- api/state.js
+++ api/state.js
@@ -3,6 +3,9 @@ const { hasSupabaseConfig, supabaseFetch } = require("./_supabase");
 function sendJson(res, status, body) {
   res.statusCode = status;
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, apikey");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.end(JSON.stringify(body));
 }
 
